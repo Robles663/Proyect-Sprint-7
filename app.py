2d39c0b574647b5415b8eaa0b4f7a7ee1c348e2f
Robles663 <robleslizarragaa132@gmail.com>
2026-01-02 22:06:02 -0800

    Agregar grafico de dispersion

--- app.py
+++ app.py
@@ -5,11 +5,18 @@ import plotly_express as px
 # Lectura del dataframe
 car_data = pd.read_csv("vehicles_us.csv")
 
-st.header("Primera aplicacion")
-hist_button = st.button("Construir histograma")
+st.header("Graficas sobre el marketing de vehiculos.")
+hist_button = st.button("Construir histograma.")
 
 if hist_button:
     st.write(
         "Creacion de un histograma para el conjunto de anuncios de venta de coches")
     fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button("Construir grafico de dispersión.")
+
+if scatter_button:
+    st.write("Creando grafico de dispersión.")
+    fig1 = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig1, use_container_width=True)
