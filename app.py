65d2449345ee7735703e779c47c3626fb44af691
Robles663 <robleslizarragaa132@gmail.com>
2026-01-03 18:31:54 -0800

    Agrega modificacion de estado

--- app.py
+++ app.py
@@ -5,28 +5,51 @@ import plotly_express as px
 # Lectura del dataframe
 car_data = pd.read_csv("vehicles_us.csv")
 
-# Creando histograma
-st.header("Graficas sobre el marketing de vehiculos.")
-hist_button = st.button("Construir histograma.")  # Crea boton
+# Funciones para modificar el estado
 
-if hist_button:
+
+def state_hist():
+    st.session_state.show_hist = True
+
+
+def state_scatter():
+    st.session_state.show_scatter = True
+
+
+def state_model_hist():
+    st.session_state.show_model = True
+
+
+# Verificar si el estado existe
+if "show_hist" not in st.session_state:
+    st.session_state.show_hist = False
+
+if "show_scatter" not in st.session_state:
+    st.session_state.show_scatter = False
+
+if "show_model" not in st.session_state:
+    st.session_state.show_model = False
+
+# Titulo
+st.header("Graficas sobre la venta de vehiculos.")
+
+# Botones
+st.button("Construir histograma", on_click=state_hist)
+st.button("Construir grafico de dispersion", on_click=state_scatter)
+st.button("Crear histograma por año del modelo", on_click=state_model_hist)
+
+# Creando histograma
+if st.session_state.show_hist:
     st.write(
         "Creacion de un histograma para el conjunto de anuncios de venta de coches")
-    fig = px.histogram(car_data, x="odometer")
-    st.plotly_chart(fig, use_container_width=True)
+    hist = px.histogram(car_data, x="odometer")
+    st.plotly_chart(hist, use_container_width=True)
+if st.session_state.show_scatter:
+    st.write("Creando grafico de dispersion")
+    scatter = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(scatter, use_container_width=True)
 
-# Creando grafico de dispersion
-scatter_button = st.button("Construir grafico de dispersión.")  # Crea boton
-
-if scatter_button:
-    st.write("Creando grafico de dispersión.")
-    fig1 = px.scatter(car_data, x="odometer", y="price")
-    st.plotly_chart(fig1, use_container_width=True)
-
-# Creando histograma del year modelo
-model_button = st.button("Histograma por año del modelo")
-
-if model_button:
-    st.write("Creando un histograma con el año de salida")
-    fig_year = px.histogram(car_data, x="model_year")
-    st.plotly_chart(fig_year, use_container_width=True)
+if st.session_state.show_model:
+    st.write("Creando histograma por año del modelo")
+    model_hist = px.histogram(car_data, x="model_year")
+    st.plotly_chart(model_hist, use_container_width=True)
